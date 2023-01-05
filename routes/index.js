5f8385bef24b451773db7bbcc60003dc8245ab88
Nicolas <hnpf2022@gmail.com>
2023-01-05 01:52:04 -0300

    changing_something_as_deploy_is_stuck

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 
 routes.get('/', (req, res) => {
-  res.send('Nicolas Pariente');
+  res.send('Nicolas Pariente Fros');
 });
 
 module.exports = routes;
