9394a4dddacc57ebdd82f53aaf542b056d6c11ac
Nicolas <hnpf2022@gmail.com>
2023-01-07 03:27:02 -0300

    fixing lesson one

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ const routes = require('express').Router();
 
 routes.use('/contacts', require('./contacts'));
 
+routes.get('/', (req, res) => {
+  res.send('Nicolas Pariente Fros');
+});
+
 module.exports = routes;
