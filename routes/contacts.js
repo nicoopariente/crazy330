61aa5f796c61254b0dd46b77ef7aea4813fe6424
Nicolas <hnpf2022@gmail.com>
2023-01-12 00:12:43 -0300

    Lesson3

--- routes/contacts.js
+++ routes/contacts.js
@@ -6,6 +6,11 @@ routes.get('/', controller.getAll);
 
 routes.get('/:id', controller.getSingle);
 
+routes.post('/', controller.createSingle);
+
+routes.put('/:id', controller.updateSingle);
+
+routes.delete('/:id', controller.deleteSingle);
 
 
 module.exports = routes;
