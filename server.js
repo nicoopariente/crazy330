ca199156b405643eabb649c6ccd01af082696bd1
Nicolas <hnpf2022@gmail.com>
2023-01-14 01:46:36 -0300

    Cors trying fixing problem

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const cors = require('cors');
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json()).use(cors({
+  origin: ['http://localhost:3000', 'https://crazy341.onrender.com/','https://cse341-contacts-frontend.netlify.app/']
+}))
 .use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
